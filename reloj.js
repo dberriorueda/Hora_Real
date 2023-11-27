1849662b5ab6b37772dedd7b61e0ba85870c3274
Diego <dberriorueda@gmail.com>
2023-11-27 12:41:26 -0500

    Organizando la hora de AM y PM

--- reloj.js
+++ reloj.js
@@ -1,15 +1,29 @@
 function displeytime() {
     var tiempo = new Date();
-    console.log("tiempo");
     var hora = tiempo.getHours();
     var minuto = tiempo.getMinutes();
     var segundo = tiempo.getSeconds();
-    var session = document.getElementById('session');
+    var session = "AM";
+
+    if(hora >= 12) {
+        session = "PM"
+        hora = hora -12
+    }
+
+    if ( hora === 0) {
+        hora = 12
+    }
 
 
 
-    document.getElementById('horas').innerHTML = hora;
-    document.getElementById('minutos').innerHTML = minuto;
-    document.getElementById('segundos').innerHTML = segundo;
+    document.getElementById('horas').innerHTML = formatTime(hora);
+    document.getElementById('minutos').innerHTML = formatTime(minuto);
+    document.getElementById('segundos').innerHTML = formatTime(segundo);
+    document.getElementById('session').innerHTML = session
 }
+
+function formatTime(time) {
+    return time <10 ? "0" + time : time
+}
+
 setInterval(displeytime, 10);
